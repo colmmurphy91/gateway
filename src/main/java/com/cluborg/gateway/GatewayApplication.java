227288d4ece152bed87d8fa284107c91f5e5d60a
Colm Murphy <colm.j.murphy91@gmail.com>
2019-06-29 18:43:11 +0400

    changing to kubernetes deployment

--- src/main/java/com/cluborg/gateway/GatewayApplication.java
+++ src/main/java/com/cluborg/gateway/GatewayApplication.java
@@ -3,14 +3,9 @@ package com.cluborg.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
 public class GatewayApplication {
 
     public static void main(String[] args) {
